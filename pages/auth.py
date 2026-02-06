12ba1717774bc527332e15a0f14d5b741d9f87b8
Ismail Usman <140979127+Ishmeeeel@users.noreply.github.com>
2026-02-06 09:57:05 +0100

    Refactor authentication functions and update UI

--- pages/auth.py
+++ pages/auth.py
@@ -1,47 +1,40 @@
-
 import streamlit as st
 from services.supabase_client import get_client
 
-def save_session(res):
+def _save_session(res):
     st.session_state["user"] = {
         "id": res.user.id,
-        "email": res.user.email,
+        "email": res.user.email
     }
     st.success("Signed in!")
 
-def login():
-    st.subheader("Sign In")
+def show_login():
+    st.subheader("Sign in")
     email = st.text_input("Email")
-    pwd = st.text_input("Password", type="password")
-
-    if st.button("Sign In"):
-        sb = get_client()
-        res = sb.auth.sign_in_with_password({"email": email, "password": pwd})
+    password = st.text_input("Password", type="password")
+    if st.button("Sign in"):
+        supabase = get_client()
+        res = supabase.auth.sign_in_with_password({"email": email, "password": password})
         if res.user:
-            save_session(res)
+            _save_session(res)
         else:
             st.error("Invalid credentials")
 
-def signup():
-    st.subheader("Create Account")
-    email = st.text_input("Email", key="s_email")
-    pwd = st.text_input("Password", type="password", key="s_pwd")
+def show_signup():
+    st.subheader("Create account")
+    email = st.text_input("Email", key="su_email")
+    password = st.text_input("Password", type="password", key="su_pwd")
+    if st.button("Sign up"):
+        supabase = get_client()
+        supabase.auth.sign_up({"email": email, "password": password})
+        st.info("Check your email to confirm, then sign in.")
 
-    if st.button("Sign Up"):
-        sb = get_client()
-        sb.auth.sign_up({"email": email, "password": pwd})
-        st.info("Account created. Check your email to confirm.")
-
-mode = st.radio("Mode", ["Sign In", "Sign Up"], horizontal=True)
-
-if mode == "Sign In":
-    login()
-else:
-    signup()
+mode = st.radio("Auth mode", ["Sign in","Sign up"], horizontal=True)
+show_login() if mode=="Sign in" else show_signup()
 
 if "user" in st.session_state:
     st.success(f"Logged in as {st.session_state['user']['email']}")
-    if st.button("Sign Out"):
+    if st.button("Sign out"):
         get_client().auth.sign_out()
         st.session_state.pop("user", None)
         st.rerun()
