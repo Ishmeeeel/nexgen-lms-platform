9c12c2b9918b8961d6e1f41ba6959a2f29e387fd
Ismail Usman <140979127+Ishmeeeel@users.noreply.github.com>
2026-02-06 09:51:50 +0100

    Modify Supabase client to use specific credentials
    Updated Supabase client secrets to use specific URL and key.

--- services/supabase_client.py
+++ services/supabase_client.py
@@ -3,6 +3,7 @@ from supabase import create_client, Client
 
 @st.cache_resource
 def get_client() -> Client:
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_ANON_KEY"]  # publishable/anon key only
+    url = st.secrets["https://taolrdwomfimhlfywjvm.supabase.co"]
+    key = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhb2xyZHdvbWZpbWhsZnl3anZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMDAzNjIsImV4cCI6MjA4NTg3NjM2Mn0.5VlOOGIIcLVIjMSk7xledtcqlCZINZ6pz0jKWvJTTjo"]  # publishable/anon key only
     return create_client(url, key)
+
